9255475f4b3d3f530401c62516dc5974837626cc
Or <orkilim@gmail.com>
2022-03-31 11:24:55 +0300

    first task done

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { fetchImageUrls } from "../api/index";
 
 const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+    const [images,setImages]=useState([])
+    const [counter,setCounter]=useState(0)
+    
+
+    
+    useEffect(()=>{
+        fetchImageUrls()
+        .then((data)=>{
+            setImages(data)
+        })
+        .catch((err)=>{
+            console.log("error in useEffect is: ",err)
+        })
+    },[])
+
+    const handleClick=(str)=>{
+        if(str=="next")
+        {
+            if(counter==images.length-1)
+            {
+                setCounter(0)
+            }
+            else{
+                setCounter(counter+1)
+            }
+        }
+        else{
+            if(counter==0)
+            {
+                setCounter(images.length-1)
+            }
+            else{
+                setCounter(counter-1)
+            }
+        }
+    }
+
+    return(
+       <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+           <button onClick={()=>{handleClick("prev")}}>previous</button>
+           <img style={{width:"50%",height:"50%"}} src={images[counter]} alt="image in carousel" />
+           <button onClick={()=>{handleClick("next")}} >next</button>
+       </div> 
+    );
 };
 export default ImageCarousel;
